577f30f0699c11cdd5a494ed30ebda8169e27419
Daniel Soto Ortiz <daniel.soto.ortiz@gmail.com>
2024-10-09 09:57:55 +0200

    ddd

---
+++ ej19.js
@@ -0,0 +1,20 @@
+window.onload = function(){
+
+    function Persona (nombre, edad, genero){
+
+        this.nombre=nombre;
+        this.edad=edad;
+        this.genero=genero;
+
+    }
+
+    function estudiante(nombre, edad, genero, curso, grupo){
+
+        
+
+
+    }
+
+
+
+}
